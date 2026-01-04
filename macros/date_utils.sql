f2235d6908dadf90f778934865fe5ffab2a7daf7
Olman Felipe Fonseca Oviedo <ofonsecao0493@ulicori.net>
2026-01-04 19:26:43 +0000

    asda

--- macros/date_utils.sql
+++ macros/date_utils.sql
@@ -1,13 +1,8 @@
-{% macro getseason(x) %}
-
+{% macro getseason(ts_col) %}
     case
-        when month(x) in (12, 1, 2)
-        then 'WINTER'
-        when month(x) in (3, 4, 5)
-        then 'SPRING'
-        when month(x) in (6, 7, 8)
-        then 'SUMMER'
+        when month({{ ts_col }}) in (12, 1, 2) then 'WINTER'
+        when month({{ ts_col }}) in (3, 4, 5) then 'SPRING'
+        when month({{ ts_col }}) in (6, 7, 8) then 'SUMMER'
         else 'AUTUMN'
     end
-
 {% endmacro %}
